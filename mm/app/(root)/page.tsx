9844721034075213bdfdca3cac257450c021eed5
muhuanlegend <muhuanlegend@gmail.com>
2024-05-06 16:48:24 +0000

    charts inserted

--- mm/app/(root)/page.tsx
+++ mm/app/(root)/page.tsx
@@ -1,8 +1,30 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName: 'Miles' }
+
   return (
-    <div>Home</div>
+    <section className='home ' >
+        <div className='home-content' >
+            <header className='home-header' >
+                <HeaderBox 
+                type='greeting'
+                title='Welcome,'
+                user={loggedIn?.firstName || 'Guest'}
+                subtext='Access and manage your account and transactions efficiently.'
+                />
+
+                <TotalBalanceBox 
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={35656.70}
+
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
