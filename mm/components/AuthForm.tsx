376e62be6049bfd0e8d225d97e3ab1a63349c381
muhuanlegend <muhuanlegend@gmail.com>
2024-05-09 13:23:02 +0000

    tested out the loggeninUser function in sign up

--- mm/components/AuthForm.tsx
+++ mm/components/AuthForm.tsx
@@ -13,13 +13,14 @@ import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { signIn, signUp } from '@/lib/actions/user.actions';
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
 
 
 const AuthForm = ({type}: {type: string}) => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
+  // const loggedInUser = await getLoggedInUser();
 
   const formSchema = authFormSchema(type);
   
@@ -45,11 +46,11 @@ const onSubmit = async (data: z.infer<typeof formSchema>) => {
     }
 
     if(type === 'sign-in') {
-      const response = await signIn({
-        email: data.email,
-        password: data.password,
-      })
-      if(response) router.push('/')
+      // const response = await signIn({
+      //   email: data.email,
+      //   password: data.password,
+      // })
+      // if(response) router.push('/')
     }
   } catch (error) {
     console.log(error);
--- mm/lib/actions/user.actions.ts
+++ mm/lib/actions/user.actions.ts
@@ -1,4 +1,9 @@
-'user server';
+'use server';
+
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
 
 export const signIn = async () => {
     try {
@@ -8,10 +13,34 @@ export const signIn = async () => {
     }
 }
 
-export const signUp = async ({userData: SignUpParams}) => {
+export const signUp = async (userData: SignUpParams) => {
+    const { email, password, firstName, lastName } = userData;
+
     try {
-       //using appwrite to create a user account 
+        const { account } = await createAdminClient();
+
+        const newUserAccount = await account.create(ID.unique(), email, password, `${firstName} ${lastName}`);
+        const session = await account.createEmailPasswordSession(email, password);
+
+        cookies().set("appwrite-session", session.secret, {
+        path: "/",
+        httpOnly: true,
+        sameSite: "strict",
+        secure: true,
+        });
+        return parseStringify(newUserAccount);
     } catch (error) {
         console.error('Error', error)
     }
 }
+
+export async function getLoggedInUser() {
+    try {
+      const { account } = await createSessionClient();
+      const user = await account.get();
+      return parseStringify(user)
+    } catch (error) {
+      return null;
+    }
+  }
+  
