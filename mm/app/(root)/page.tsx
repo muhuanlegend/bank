b19c179eeb27baf52fc03fde4d2fa96d1f9be82e
muhuanlegend <muhuanlegend@gmail.com>
2024-05-21 15:44:48 +0000

    am tired

--- mm/app/(root)/page.tsx
+++ mm/app/(root)/page.tsx
@@ -1,11 +1,22 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import {  getAccount, getAccounts } from '@/lib/actions/bank.actions'
 import { getLoggedInUser } from '@/lib/actions/user.actions'
 import React from 'react'
+import RecentTransactions from '@/components/RecentTransactions'
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page} }: SearchParamProps) => {
+    const currentPage = Number(page as string) || 1;
     const loggedIn = await getLoggedInUser();
+    const accounts = await getAccounts({ userId: loggedIn.$id })
+    const accountsData = accounts?.data;
+    
+    
+    if(!accounts) return;
+
+    const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+    const account = await getAccount({ appwriteItemId })
 
   return (
     <section className='home ' >
@@ -14,23 +25,29 @@ const Home = async () => {
                 <HeaderBox 
                 type='greeting'
                 title='Welcome,'
-                user={loggedIn?.name || 'Guest'}
+                user={loggedIn?.firstName || 'Guest'}
                 subtext='Access and manage your account and transactions efficiently.'
                 />
 
                 <TotalBalanceBox 
-                accounts={[]}
-                totalBanks={1}
-                totalCurrentBalance={35656.70}
+                accounts={accountsData}
+                totalBanks={accounts?.totalBanks}
+                totalCurrentBalance={accounts?.totalCurrentBalance}
 
                 />
             </header>
             {/* Recent transactions */}
+            <RecentTransactions
+            accounts={accountsData}
+            transactions={account?.transactions}
+            appwriteItemId={appwriteItemId}
+            page={currentPage}
+            />
         </div>
         <RightSidebar 
         user={loggedIn}
-        transactions={[]}
-        banks={[{currentBalance: 8956.02}, {currentBalance: 56.02}]}
+        transactions={accounts?.transactions}
+        banks={accountsData?.slice(0, 2)}
         />
     </section>
   )
--- mm/components/Sidebar.tsx
+++ mm/components/Sidebar.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import Footer from './Footer'
+import PlaidLink from './PlaidLink'
 
 const Sidebar = ({ user }: SiderbarProps) => {
     const pathname = usePathname();
@@ -51,6 +52,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
                 )
             })}
             {/* User */}
+            <PlaidLink user={user} />
         </nav>
         {/* footer */}
         <Footer user={user}/>
